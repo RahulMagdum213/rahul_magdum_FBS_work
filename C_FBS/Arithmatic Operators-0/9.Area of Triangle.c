5e3967a5a31efd8e781b1ca6542d7f760f49d4cd
RahulMagdum213 <rahulbmagdum08@gmail.com>
2026-02-12 11:26:37 +0530

    Fixing

--- C_FBS/Arithmatic Operators-0/9.Area of Triangle.c
+++ C_FBS/Arithmatic Operators-0/9.Area of Triangle.c
@@ -7,7 +7,7 @@
  	b=3;
  	h=6;
  	a=0.5*b*h;
- 	printf("Area of trianglr having base = %d and height =%d is %d",b,h,a);
+ 	printf("Area of triangle having base = %d and height =%d is %d",b,h,a);
  	//taked 0.5 cause as in c if i take 1/2 its doint first int/int = int answer which is 1/2=0 so always take 0.5
  	
  }
